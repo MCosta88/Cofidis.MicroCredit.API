effa6dccb53aa9100c51fbf3d37f923d60bd274c
MCosta88 <57393821+MCosta88@users.noreply.github.com>
2024-10-24 00:03:11 +0100

    Add files via upload

---
+++ sp_CalcCreditLimit.sql
@@ -0,0 +1,18 @@
+CREATE PROCEDURE sp_CalcCreditLimit
+ @baseSalary DECIMAL(18, 2),
+ @creditLimit DECIMAL(18, 2) OUTPUT
+AS
+BEGIN
+    IF @baseSalary <= 1000
+    BEGIN
+        SET @creditLimit = 1000;
+    END
+    ELSE IF @baseSalary > 1000 AND @baseSalary <= 2000
+    BEGIN
+        SET @creditLimit = 2000;
+    END
+    ELSE
+    BEGIN
+        SET @creditLimit = 5000;
+    END
+END
